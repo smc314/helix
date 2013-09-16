7eb14d71ae1214456199da834f592e6a7388fce6
Steven M. Cherry <smc@cherrys.org>
2013-09-16 17:21:05 -0500

    Initial dev application skeleton.

--- qd/dev/source/class/dev/Application.js
+++ qd/dev/source/class/dev/Application.js
@@ -12,6 +12,10 @@
 
 #asset(dev/test.png)
 #asset(dev/*)
+#asset(dev/icon/128x128/shadow/components.png)
+#asset(dev/icon/128x128/shadow/data_gear.png)
+#asset(dev/icon/128x128/shadow/window_sidebar.png)
+#asset(dev/icon/128x128/shadow/server_client_exchange.png)
 
 ************************************************************************ */
 
@@ -20,57 +24,73 @@
  */
 qx.Class.define("dev.Application",
 {
-  extend : qx.application.Standalone,
+	extend : qx.application.Standalone,
 
 
 
-  /*
-  *****************************************************************************
-     MEMBERS
-  *****************************************************************************
-  */
+	/*
+	*****************************************************************************
+	MEMBERS
+	*****************************************************************************
+	*/
 
-  members :
-  {
-    /**
-     * This method contains the initial application code and gets called
-     * during startup of the application
-     *
-     * @lint ignoreDeprecated(alert)
-     */
-    main : function()
-    {
-      // Call super class
-      this.base(arguments);
+	members :
+	{
+		/**
+		* This method contains the initial application code and gets called
+		* during startup of the application
+		*
+		* @lint ignoreDeprecated(alert)
+		*/
+		main : function() {
+			// Call super class
+			this.base(arguments);
 
-      // Enable logging in debug variant
-      if (qx.core.Environment.get("qx.debug"))
-      {
-        // support native logging capabilities, e.g. Firebug for Firefox
-        qx.log.appender.Native;
-        // support additional cross-browser console. Press F7 to toggle visibility
-        qx.log.appender.Console;
-      }
+			// Enable logging in debug variant
+			if (qx.core.Environment.get("qx.debug"))
+			{
+				// support native logging capabilities, e.g. Firebug for Firefox
+				qx.log.appender.Native;
+				// support additional cross-browser console. Press F7 to toggle visibility
+				qx.log.appender.Console;
+				// support logging back to the helix server
+				//dev.LogAppender;
+			}
 
-      /*
-      -------------------------------------------------------------------------
-        Below is your actual application code...
-      -------------------------------------------------------------------------
-      */
+			// Setup the application skeleton
+			this.setupInitialLayout();
+		},
 
-      // Create a button
-      var button1 = new qx.ui.form.Button("First Button", "dev/test.png");
+		setupInitialLayout : function() {
+			var doc = this.getRoot();
+			this.rootTabView = new qx.ui.tabview.TabView;
+			this.rootTabView.setBarPosition( "right" );
+			doc.add( this.rootTabView, {left: 0, top: 0, right: 0, bottom: 0} );
 
-      // Document is the application root
-      var doc = this.getRoot();
+			var largeFont = new qx.bom.Font( 30 );
+			var tabPage = dev.Statics.addTabPage( this.rootTabView, "My Projects", false, true );
+			var tabButton = tabPage.getChildControl( "button" );
+			tabPage.setIcon( "dev/icon/128x128/shadow/components.png" );
+			tabButton.setFont( largeFont );
+			tabButton.setIconPosition( "top" );
 
-      // Add button to document at fixed coordinates
-      doc.add(button1, {left: 100, top: 50});
+			tabPage = dev.Statics.addTabPage( this.rootTabView, "My Data", false, false );
+			tabButton = tabPage.getChildControl( "button" );
+			tabPage.setIcon( "dev/icon/128x128/shadow/data_gear.png" );
+			tabButton.setFont( largeFont );
+			tabButton.setIconPosition( "top" );
 
-      // Add an event listener
-      button1.addListener("execute", function(e) {
-        alert("Hello World!");
-      });
-    }
-  }
+			tabPage = dev.Statics.addTabPage( this.rootTabView, "My Screens", false, false );
+			tabButton = tabPage.getChildControl( "button" );
+			tabPage.setIcon( "dev/icon/128x128/shadow/window_sidebar.png" );
+			tabButton.setFont( largeFont );
+			tabButton.setIconPosition( "top" );
+
+			tabPage = dev.Statics.addTabPage( this.rootTabView, "My API's", false, false );
+			tabButton = tabPage.getChildControl( "button" );
+			tabPage.setIcon( "dev/icon/128x128/shadow/server_client_exchange.png" );
+			tabButton.setFont( largeFont );
+			tabButton.setIconPosition( "top" );
+		}
+	}
 });
